bc3f86b33356ae3a9ba9648fc12d6769031b75b3
nana <ncnana.608738752000b@gmail.com>
2020-05-10 13:37:30 +0900

    Add Employee create

--- src/listeners/PropertiesListener.java
+++ src/listeners/PropertiesListener.java
@@ -12,31 +12,16 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
-/**
- * Application Lifecycle Listener implementation class PropertiesListener
- *
- */
 @WebListener
 public class PropertiesListener implements ServletContextListener {
 
-    /**
-     * Default constructor.
-     */
     public PropertiesListener() {
-        // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
-     */
-    public void contextDestroyed(ServletContextEvent arg0)  {
-         // TODO Auto-generated method stub
+    public void contextDestroyed(ServletContextEvent arg0) {
     }
 
-    /**
-     * @see ServletContextListener#contextInitialized(ServletContextEvent)
-     */
-    public void contextInitialized(ServletContextEvent arg0)  {
+    public void contextInitialized(ServletContextEvent arg0) {
         ServletContext context = arg0.getServletContext();
 
         String path = context.getRealPath("/META-INF/application.properties");
@@ -47,12 +32,13 @@ public class PropertiesListener implements ServletContextListener {
             is.close();
 
             Iterator<String> pit = properties.stringPropertyNames().iterator();
-            while(pit.hasNext()) {
+            while (pit.hasNext()) {
                 String pname = pit.next();
                 context.setAttribute(pname, properties.getProperty(pname));
             }
-        } catch(FileNotFoundException e) {
-        } catch(IOException e) {}
+        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
+        }
     }
 
 }
